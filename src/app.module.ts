83759147adb8da2a99c85d19d1f036893a557afa
spiffy1 <phann8813@gmail.com>
2021-04-12 15:22:51 +0700

    Init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ImageModule } from './image/image.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [ImageModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
